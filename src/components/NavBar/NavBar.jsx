0a27ceb99e8c5e6a130ec1e1ea1030b80bc7d7b3
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 09:38:02 -0400

    Imported useContext and UserContext into our NavBar dot jsx file.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,28 @@
+import { useContext } from "react"
 import { Link } from "react-router"
 
+import { UserContext } from "../../contexts/UserContext"
+
 const NavBar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/sign-up'>Sign up</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  const { user } = useContext(UserContext)
+
+  return (
+    <nav>
+      {user ? (
+        <ul>
+          <li>
+            Welcome, {user.username}
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to='/sign-up'>Sign up</Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  )
 }
 
 export default NavBar
