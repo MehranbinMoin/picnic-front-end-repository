8510729a4dc0610b3848760ad1b917297f332a87
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 11:12:40 -0400

    Added useState and appropriate map method on the Dashboard dot jsx file to display all baskets available in the local area.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,37 @@
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
+import { useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import * as UserService from "../../services/userService"
 
 const Dashboard = () => {
-    const { user } = useContext(UserContext)
+  const { user } = useContext(UserContext);
+  const [fetchedUsers, setFetchedUsers] = useState([])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const fetchedData = await UserService.index()
+                console.log(fetchedData);
+                setFetchedUsers(fetchedData)
+            } catch (err) {
+                console.log(err);
+
+            }
+        }
+        if (user) fetchUsers()
+    }, [user])
+
     return (
         <main>
             <h1>Make or take a basket, {user.username}!</h1>
             <p>
                 Here are all available baskets in your area.
             </p>
+              <dt>
+              {fetchedUsers.map((user, idx) => (
+                    <li key={idx}>{user.username}</li>
+                ))}
+              </dt>
         </main>
     )
 }
