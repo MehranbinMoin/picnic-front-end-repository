1af36c0ee492428c9ddf3ac57e6bd5b90fe14d53
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 08:26:06 -0400

    Completed the code for proper sign up functionality on SignUpForm dot js file.

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import { signUp } from "../../services/authService";
 
 const SignUpForm = () => {
     const navigate = useNavigate()
@@ -17,7 +18,12 @@ const SignUpForm = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault()
-        console.log(formData);
+        try {
+            const newUser = await signUp(formData)
+            console.log(newUser);
+        } catch (err) {
+            setMessage(err.message)
+        }
     }
 
     const isFormInvalid = () => {
