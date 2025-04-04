8006e3db51d09ed7c1c920a4471e9d0f5bcc03ac
mehranbinmoin <mehranmoin@gmail.com>
2025-04-04 08:42:45 -0400

    Imported UserProvider into the main dor jsx file and wrapped the entire app inside of the UserProvider component.

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+    return (
+        <UserContext.Provider>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export {
+    UserProvider,
+
+}
