13dfea661359db92c9e3a5bc4d82a9eefa5dd5c6
mehranbinmoin <mehranmoin@gmail.com>
2025-04-05 09:11:40 -0400

    Building out the BasketForm so that users can create their own baskets on the platform.

--- src/components/BasketForm/BasketForm.jsx
+++ src/components/BasketForm/BasketForm.jsx
@@ -1,6 +1,79 @@
+import { useState } from "react"
 
 const BasketForm = (props) => {
-    return <main>Basket Form</main>
+    const [formData, setFormData] = useState({
+        image: '',
+        title: '',
+        description: '',
+        city: '',
+        email: '',
+    })
+
+    const handleSubmit = (event) => {
+
+    }
+
+    const handleChange = (event) => {
+
+    }
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="image">Photo</label>
+                <input
+                    type="text"
+                    required
+                    name="image"
+                    id="image"
+                    value={formData.image}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="title">Basket Title</label>
+                <input
+                    type="text"
+                    required
+                    name="title"
+                    id="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="description">Basket Description</label>
+                <textarea
+                    type="text"
+                    required
+                    name="description"
+                    id="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="city">City Location</label>
+                <input
+                    type="text"
+                    required
+                    name="city"
+                    id="city"
+                    value={formData.city}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    required
+                    name="email"
+                    id="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <button type="submit">Create Basket</button>
+            </form>
+        </main>
+    )
 }
 
 export default BasketForm
