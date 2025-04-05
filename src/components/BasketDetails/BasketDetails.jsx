7a616c1e66936333dad45367425490037ae285e6
mehranbinmoin <mehranmoin@gmail.com>
2025-04-05 08:09:53 -0400

    Building out the BasketDetails dot jsx file so that when you click on an individual basket you can see all of the details associated with the basket.

--- src/components/BasketDetails/BasketDetails.jsx
+++ src/components/BasketDetails/BasketDetails.jsx
@@ -1,10 +1,42 @@
 import { useParams } from "react-router"
+import { useState, useEffect } from "react";
+import * as basketService from '../../services/basketService'
 
 const BasketDetails = () => {
     const { basketId } = useParams()
+    const [basket, setBasket] = useState(null)
+
     console.log('basket id is:', basketId);
-    
-    return <main>Basket Details</main>
+
+    useEffect(() => {
+
+        const fetchBasket = async () => {
+            const basketData = await basketService.show(basketId)
+            setBasket(basketData)
+        }
+        fetchBasket()
+    }, [basketId])
+
+    console.log('basket state', basket);
+
+    if (!basket) return <main>Loading...</main>
+
+    return (
+        <main>
+            <section>
+                <header>
+                    <p>{basket.image}</p>
+                    <h3>{basket.title}</h3>
+                    <h3>{basket.description}</h3>
+                    <h3>{basket.city}</h3>
+                    <h3>{basket.email}</h3>
+                    <p>
+                        {`${basket.author.username} created this basket on ${new Date(basket.createdAt).toLocaleDateString()}`}
+                    </p>
+                </header>
+            </section>
+        </main>
+    )
 }
 
 export default BasketDetails
