e61090d91abd32515b81cd635dbbf5bbd04cad10
mehranbinmoin <mehranmoin@gmail.com>
2025-04-07 14:03:24 -0400

    Styling the create basket form.

--- src/components/BasketForm/BasketForm.jsx
+++ src/components/BasketForm/BasketForm.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router"
 import * as basketService from '../../services/basketService'
 import UploadWidget from "../UploadWidget";
+import styles from './BasketForm.module.css'
 
 
 const BasketForm = (props) => {
@@ -43,9 +44,10 @@ return (
         <form onSubmit={handleSubmit}>
             <label>Photo of basket</label>
             <UploadWidget formData={formData} setFormData={setFormData}/>
-
+            <br></br>
             <label htmlFor="title">Basket Title</label>
             <input
+                className={styles.formInput}
                 type="text"
                 required
                 name="title"
@@ -53,9 +55,10 @@ return (
                 value={formData.title}
                 onChange={handleChange}
             />
-
+            <br></br>
             <label htmlFor="description">Basket Description</label>
             <textarea
+                className={styles.formInput}
                 type="text"
                 required
                 name="description"
@@ -63,9 +66,10 @@ return (
                 value={formData.description}
                 onChange={handleChange}
             />
-
+            <br></br>
             <label htmlFor="city">City Location</label>
             <input
+                className={styles.formInput}
                 type="text"
                 required
                 name="city"
@@ -73,9 +77,10 @@ return (
                 value={formData.city}
                 onChange={handleChange}
             />
-
+            <br></br>
             <label htmlFor="email">Email</label>
             <input
+                className={styles.formInput}
                 type="text"
                 required
                 name="email"
@@ -83,8 +88,8 @@ return (
                 value={formData.email}
                 onChange={handleChange}
             />
-
-            <button type="submit">Create Basket</button>
+            <br></br>
+            <button className={styles.button} type="submit">Create Basket</button>
         </form>
     </main>
 )
