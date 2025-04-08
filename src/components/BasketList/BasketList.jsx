3308c896c1206e98b074710905e18c98d8f87ca8
mehranbinmoin <mehranmoin@gmail.com>
2025-04-08 11:26:38 -0400

    Styling the basket list page so that individual basket cards are uniform in shape and size.

--- src/components/BasketList/BasketList.jsx
+++ src/components/BasketList/BasketList.jsx
@@ -2,25 +2,27 @@ import { Link } from "react-router"
 import styles from './BasketList.module.css'
 
 const BasketList = (props) => {
-    console.log(props);
-    
+
     return (
         <main>
             <div className={styles.CardContainer}>
-            {props.baskets.map((basket) => (
-                <Link className={styles.IndividualCard} key={basket._id} to={`/baskets/${basket._id}`}>
-                    <article>
-                        <header className={styles.Header}>
-                            <img className={styles.BasketImage} src={basket.image} alt={`Image of ${basket.description}`}></img>
-                            <h2>{basket.description}</h2>
-                            <p>
-                                {`${basket.author.username} created this basket on ${new Date(basket.createdAt).toLocaleDateString()}`}
-                            </p>
-                        </header>
-                        <p>{basket.title}</p>
-                    </article>
-                </Link>
-            ))}
+                {props.baskets.map((basket) => (
+                    <Link className={styles.IndividualCard} key={basket._id} to={`/baskets/${basket._id}`}>
+                        <article className={styles.Card}>
+                            <header className={styles.Header}>
+                                <img className={styles.BasketImage}
+                                    src={basket.image}
+                                    alt={`Image of ${basket.description}`}
+                                />
+                                <h3>{basket.title}</h3>
+                                <h4>{basket.description}</h4>
+                                <p>
+                                    {`${basket.author.username} created this basket on ${new Date(basket.createdAt).toLocaleDateString()}`}
+                                </p>
+                            </header>
+                        </article>
+                    </Link>
+                ))}
             </div>
             <footer>© 2025 picnic</footer>
         </main>
