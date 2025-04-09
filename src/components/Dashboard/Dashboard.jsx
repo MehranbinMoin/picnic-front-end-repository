5a009bb50895f00f51fd8247fb07e3050fdb769a
mehranbinmoin <mehranmoin@gmail.com>
2025-04-08 20:57:05 -0400

    Added founder video onto the platform dashboard.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,18 +2,26 @@ import { useEffect, useContext } from "react"
 import { useState } from "react";
 import { UserContext } from "../../contexts/UserContext"
 import DashboardLogo from "../../assets/Photos/DashboardLogo.png"
+import styles from './Dashboard.module.css'
 
 
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
 
-
+const youtubeVideo = 'hIIzBwenOAs'
   return (
-    <main>
-      <h1>Make or take a basket, {user.username}!</h1>
-      <div className="LogoContainer">
-        <img src={DashboardLogo} className="DashboardLogo"></img>
+    <main className={styles.Main}>
+      <h1>A word from our founder!</h1>
+      <div className={styles.videoContainer}>
+        <iframe 
+          className={styles.videoIframe}
+          src={`https://www.youtube.com/embed/${'hIIzBwenOAs'}`}
+          title="Founder Message"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
       <footer>© 2025 picnic</footer>
     </main>
